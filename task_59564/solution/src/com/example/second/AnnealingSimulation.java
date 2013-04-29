b00c6903f483fce48b6b5638bcd9b0cbb3423ebd
oor1@ukr.net <oleg>
2013-04-29 21:29:06 +0300

    refactoring

---
+++ task_59564/solution/src/com/example/second/AnnealingSimulation.java
@@ -0,0 +1,24 @@
+package com.example.second;
+
+abstract class AnnealingSimulation{
+
+    private SingleVariableFunction function;
+
+    protected AnnealingSimulation(SingleVariableFunction function) {
+        this.function = function;
+    }
+
+    public double search(){
+        //todo
+
+        return 0;
+    }
+
+    protected abstract double nextState(double state);
+
+    protected abstract double cooling(double temperature);
+
+    protected abstract double probability(double oldStateEnergy, double newStateEnergy, double temperature);
+
+
+}
