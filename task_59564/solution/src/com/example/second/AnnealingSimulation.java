e12873f6d669308cb886720d10f959afc30faaed
oleg.oginskiy <oleg@themidnightcoders.com>
2013-04-30 16:10:02 +0300

    task 59564. implemented method search

--- task_59564/solution/src/com/example/second/AnnealingSimulation.java
+++ task_59564/solution/src/com/example/second/AnnealingSimulation.java
@@ -4,21 +4,64 @@ abstract class AnnealingSimulation{
 
     private SingleVariableFunction function;
 
+    private double minimalTemperature = 0.1;
+    private int maxIteration = Integer.MAX_VALUE;
+
     protected AnnealingSimulation(SingleVariableFunction function) {
         this.function = function;
     }
 
     public double search(){
-        //todo
+        //init state
+        double currentState = initState();
+        int iteration = 0;
+        double temperature = cooling(iteration++);
 
-        return 0;
+        do {
+            double nextState = nextState(currentState, temperature);
+
+            double currentStateEnergy = function.evaluate(currentState);
+            double nextStateEnergy = function.evaluate(nextState);
+
+            if(Math.random() > probability(currentStateEnergy, nextStateEnergy, temperature))
+                currentState = nextState;
+
+            temperature = cooling(iteration++);
+        }while ( temperature > minimalTemperature|| iteration < maxIteration );
+
+        return currentState;
+    }
+
+    public double getMinimalTemperature() {
+        return minimalTemperature;
+    }
+
+    public void setMinimalTemperature(double minimalTemperature) {
+        this.minimalTemperature = minimalTemperature;
+    }
+
+    public int getMaxIteration() {
+        return maxIteration;
+    }
+
+    public void setMaxIteration(int maxIteration) {
+        this.maxIteration = maxIteration;
+    }
+
+    public SingleVariableFunction getFunction() {
+        return function;
+    }
+
+    public void setFunction(SingleVariableFunction function) {
+        this.function = function;
     }
 
     protected abstract double nextState(double state, double temperature);
 
     protected abstract double cooling(double temperature);
 
-    protected abstract double probability(double oldStateEnergy, double newStateEnergy, double temperature);
+    protected abstract double probability(double currentStateEnergy, double nextStateEnergy, double temperature);
 
+    protected abstract double initState();
 
 }
